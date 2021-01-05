2cc722fde6d669148841acefb73b6cfb1c9c1d12
LAVANYAlavanya14 <lavanyaravi14011989@gmail.com>
2021-01-05 18:57:56 +0530

    initial commit

---
+++ dbutilities.java
@@ -0,0 +1,5 @@
+Hi we are in db utilites java file
+which it is the first file to create
+Hello ... Hello
+just enjoying the using of git in windows OS
+bye
---
+++ stringutilities.java
@@ -0,0 +1,6 @@
+Hello
+This is string utilities file shell scripting language.
+Now we are adding the file    working area
+after that we move the file to staging area 
+and finally we commit it local repository which is created by git init command.
+bye
