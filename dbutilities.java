7360688f7cd359b7eb6bccac296a2df8bb99ffd5
LAVANYAlavanya14 <lavanyaravi14011989@gmail.com>
2021-01-05 20:51:08 +0530

    Reverted...Revert "updated files"
    This reverts commit f754b056e30477834dade7a4f2582d6901e17dda.

--- dbutilities.java
+++ dbutilities.java
@@ -3,7 +3,3 @@ which it is the first file to create
 Hello ... Hello
 just enjoying the using of git in windows OS
 bye
-
-Iam adding extra code to the dbutilities file
-and quiting it
-
--- testchange.sh
+++
@@ -1,5 +0,0 @@
-#To test the change in  wheather it is moving from one area to other area
-Hello guys
-good morning or good afternoon or good evening
-after doing the work bye and good night
-
